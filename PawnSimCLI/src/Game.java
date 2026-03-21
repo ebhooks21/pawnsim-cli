cd0659ebd2ef8b6249ad3088b06dfe8e2506bcba
Eric Hooks <ebhooks21@gmail.com>
2026-03-20 23:57:32 -0500

    Added a Window class to track window information.

--- PawnSimCLI/src/Game.java
+++ PawnSimCLI/src/Game.java
@@ -4,31 +4,16 @@
  * Purpose: Main class for the Game.
  */
 
-import java.io.IOException;
-
-import com.googlecode.lanterna.terminal.Terminal;
-import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
-import com.googlecode.lanterna.terminal.swing.SwingTerminalFrame;
 
 public class Game {
+	//Variable for the game window
+	private Window win;
+
 	/**
 	 * Main constructor for the Game class.
 	 */
 	public Game() {
-		try {
-			Terminal terminal = new DefaultTerminalFactory().createSwingTerminal();
-
-			SwingTerminalFrame frame = (SwingTerminalFrame) terminal;
-			frame.setTitle("Pawn Shop Simulator");
-			frame.setSize(800, 600);
-			frame.setVisible(true);
-
-			while (true) { 
-				
-			}
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		//Start the game
+		win = new Window(800, 600);
 	}
 }
