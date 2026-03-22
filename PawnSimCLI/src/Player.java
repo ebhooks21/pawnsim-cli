8b042efd26ba2e576a4b7f36d142f7fc45c4edeb
Eric Hooks <ebhooks21@gmail.com>
2026-03-22 00:09:49 -0500

    Created the Player class.

---
+++ PawnSimCLI/src/Player.java
@@ -0,0 +1,24 @@
+/**
+ * Filename: Player.java
+ * Author: Eric Hooks, 2026
+ * Purpose: To define the Player.
+ */
+public class Player {
+    //Variable for the player's name
+    private String playerName;
+
+    //Variable for the player's shop name
+    private String pawnShopName;
+
+    //Variable for the player's money
+    private double shopMoney;
+
+    /**
+     * Main constructor for the player class.
+     */
+    public Player(String pName, String sName) {
+        playerName = pName;
+        pawnShopName = sName;
+        shopMoney = 0.00;
+    }
+}
